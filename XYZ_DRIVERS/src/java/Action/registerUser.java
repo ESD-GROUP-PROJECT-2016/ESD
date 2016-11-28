c4661905a637f4b39fc12b56d9ac586cf739f35e
MadelinTom <tom.madelin@gmail.com>
2016-11-28 16:56:54 +0000

    added Register user controller

---
+++ XYZ_DRIVERS/src/java/Action/registerUser.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Action;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Tom
+ */
+public class registerUser extends HttpServlet {
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ParseException, ClassNotFoundException, SQLException {
+        
+    }
+    
+}

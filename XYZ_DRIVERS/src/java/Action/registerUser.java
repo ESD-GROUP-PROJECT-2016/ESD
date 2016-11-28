60d08a4dfa0766d06b7072923ac88c5dd37d6e16
MadelinTom <tom.madelin@gmail.com>
2016-11-28 17:31:22 +0000

    register user

--- XYZ_DRIVERS/src/java/Action/registerUser.java
+++ XYZ_DRIVERS/src/java/Action/registerUser.java
@@ -8,6 +8,8 @@ package Action;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,9 +25,19 @@ public class registerUser extends HttpServlet {
             throws ServletException, IOException, ParseException, ClassNotFoundException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         
+        String userName = request.getParameter("txtUserName");
         String firstName = request.getParameter("txtFirstName");
         String secondName = request.getParameter("txtlastname");
+        String address1 = request.getParameter("txtaddressln1");
+        String address2 = request.getParameter("txtaddressln2");
+        String postcode = request.getParameter("txtpostcode");
+        String dob = request.getParameter("txtdob");
+        String password = request.getParameter("txtpassword");
         
+        SimpleDateFormat dobFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date dateOfBirth = dobFormat.parse(dob);
+        
+        Date registrationDate = new Date();
     }
     
 }

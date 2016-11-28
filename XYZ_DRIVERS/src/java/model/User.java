a385680efb8b9e761607c0f737dee0c9d02bf5a2
Samuel Arogundade <sam123uru@gmail.com>
2016-11-28 17:47:17 +0000

    user class for the model

---
+++ XYZ_DRIVERS/src/java/model/User.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author sam123uru
+ */
+public class User {
+
+    private String id;
+    private String status;
+    private String password;
+    
+    public User(){
+        
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", password=" + password + ", status=" + status + '}';
+    }
+    
+}
+    
+

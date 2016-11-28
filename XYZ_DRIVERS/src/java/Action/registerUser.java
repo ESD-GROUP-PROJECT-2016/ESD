2bb819568139ed2569d6661fdad2c56f5c465563
MadelinTom <tom.madelin@gmail.com>
2016-11-28 17:44:51 +0000

    register user

--- XYZ_DRIVERS/src/java/Action/registerUser.java
+++ XYZ_DRIVERS/src/java/Action/registerUser.java
@@ -5,6 +5,7 @@
  */
 package Action;
 
+import model.Member;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.text.ParseException;
@@ -25,7 +26,7 @@ public class registerUser extends HttpServlet {
             throws ServletException, IOException, ParseException, ClassNotFoundException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         
-        String userName = request.getParameter("txtUserName");
+        //Pull data from session
         String firstName = request.getParameter("txtFirstName");
         String secondName = request.getParameter("txtlastname");
         String address1 = request.getParameter("txtaddressln1");
@@ -34,10 +35,25 @@ public class registerUser extends HttpServlet {
         String dob = request.getParameter("txtdob");
         String password = request.getParameter("txtpassword");
         
+        //Convert date of birth into Date format
         SimpleDateFormat dobFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date dateOfBirth = dobFormat.parse(dob);
         
+        //Get registration date
         Date registrationDate = new Date();
+        
+        //Create user name based on real name
+        String initial = firstName.substring(0, 1);
+        String userId = (initial + "-" + secondName).toLowerCase();
+        
+        //Add data to new Member instance
+        Member mem = new Member();
+        mem.setName(firstName + " " + secondName);
+        mem.setAddress(address1 + "\n" + address2 + "\n" + postcode);
+        mem.setDOB(dateOfBirth);
+        mem.setDOR(registrationDate);
+        mem.setStatus("APPLIED");
+        mem.setBalance(1);
     }
     
 }

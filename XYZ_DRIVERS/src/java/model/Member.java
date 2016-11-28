10fe97956f2cc334975fe3237b3ae7d31732a186
twhite2219 <twhite_96@live.co.uk>
2016-11-28 18:31:53 +0000

    Revert "finalised base Jsp Pages"
    This reverts commit bc139d8dc717557604244685456aa3a30de93b34.

---
+++ XYZ_DRIVERS/src/java/model/Member.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import java.util.Date;
+
+/**
+ *
+ * @author Tom
+ */
+public class Member {
+    private String id;
+    private String name;
+    private String address;
+    private Date dob;
+    private Date dor; //Date of registration
+    private String status;
+    private float balance;
+    
+    public Member() {
+        
+    }
+    
+    //Set variables
+    public void setID(String ID) {
+        this.id = ID;
+    }
+    public void setName(String Name) {
+        this.name = Name;
+    }
+    public void setAddress(String Address) {
+        this.address = Address;
+    }
+    public void setDOB(Date DOB) {
+        this.dob = DOB;
+    }
+    public void setDOR(Date DOR) {
+        this.dor = DOR;
+    }
+    public void setStatus(String Status) {
+        this.status = Status;
+    }
+    public void setBalance(float Balance) {
+        this.balance = Balance;
+    }
+    
+    //Get variables
+    public String getID() {
+        return this.id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getAddress() {
+        return this.address;
+    }
+    public Date getDOB() {
+        return this.dob;
+    }
+    public Date getDOR() {
+        return this.dor;
+    }
+    public String getStatus() {
+        return this.status;
+    }
+    public float getBalance() {
+        return this.balance;
+    }
+    
+    @Override
+    public String toString() {
+        return "Member{" + "name=" + name + ", address=" + address + ", dob=" + dob + ", dor=" + dor + ", status=" + status + ", balance=" + balance + '}';
+    }
+}

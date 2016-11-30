75914fa566623046451866dbd71f98e03519bba1
twhite2219 <twhite_96@live.co.uk>
2016-11-30 01:49:39 +0000

    added user filter class - see practical 10 for details

---
+++ XYZ_ASSOC_V2/src/java/Filters/User_Filter.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+/**
+ *
+ * @author TOBY WHITE
+ */
+public class User_Filter implements Filter{
+    private FilterConfig FilterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.FilterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpReq = (HttpServletRequest) request;
+        HttpServletResponse httpResp = (HttpServletResponse) response;
+
+        HttpSession session = httpReq.getSession(false);
+
+        if (session != null) {
+            if (session.getAttribute("user-authenticated") == null) {
+                httpResp.sendRedirect("login-error.jsp");
+            } else {
+                chain.doFilter(request, response);
+            }
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
